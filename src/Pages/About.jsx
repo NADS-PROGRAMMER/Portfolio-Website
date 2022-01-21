9b4d82d7f6983f8686b35621826f0846b9cace4f
Adrian Nads <marcelonads@gmail.com>
2022-01-21 16:15:13 +0800

    CHANGED the bg of the photo in About
    Added a Skills section to About

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import { ReactComponent as AboutLogo } from '../assets/logos/AboutLogo.svg'
+import Skills from '../components/Skills'
+
 function About() {
 
   /** Each section must be in a seperate file */
   return (
-      <main className="flex flex-col items-center p-[5rem_1rem]">
-          <h1 className="heading-1">About</h1>
+      <main className="flex flex-col items-center p-[5rem_1rem] gap-[1rem] lg:grid lg:grid-cols-2 lg:place-content-center lg:place-items-center lg:max-w-[70rem] lg:mx-auto lg:gap-[5rem]">
+          <h1 className="heading-1 lg:hidden">About</h1>
 
-          <AboutLogo className="img-main p-[1rem]"/>
+          <AboutLogo className="img-main p-[1rem] lg:max-w-[26rem]"/>
 
-          {/* Brief Intro */}
-          <div className="text-center flex flex-col items-center gap-[.5rem]">
-              <section>
-                <h2 className="font-[Poppins] font-medium text-xl">Adrian Nads L. Marcelo</h2>
-                <span className="font-[Poppins]">Web Developer</span>
-              </section>
+          <div className="flex flex-col items-center gap-[2rem] lg:items-start">
               
-              <p className="font-[Poppins]">
-                I'm a passionate Web Developer from the Philippines. I'm currently
-                in my 3rd-year taking BS Computer Science. I always love creating
-                projects to showcase what I am currently learned. Another is that
-                I always documenting my journey through writing blogs.
-             </p>
+                <h1 className="hidden heading-1 lg:block text-left">About</h1>
+                {/* Brief Intro */}
+                <div className="text-center flex flex-col items-center gap-[.5rem] lg:items-start lg:text-left">
+                    <section>
+                        <h2 className="font-[Poppins] font-medium text-3xl">Adrian Nads L. Marcelo</h2>
+                        <span className="font-[Poppins] text-xl font-normal">Web Developer</span>
+                    </section>
+                    
+                    <p className="font-[Poppins] md:max-w-[55ch]">
+                        I'm a passionate Web Developer from the Philippines. I'm currently
+                        in my 3rd-year taking BS Computer Science. I always love creating
+                        projects to showcase what I am currently learned. Another is that
+                        I always documenting my journey through writing blogs.
+                    </p>
+                </div>
+
+                {/* Skills */}
+                <div className="flex flex-col items-center gap-[1rem] lg:items-start">
+                    <h2 className="text-3xl">Skills</h2>
+                    <Skills />
+                </div>
           </div>
+         
       </main>
   )
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {ReactComponent as HTML} from '../assets/Skills/html5.svg'
+import {ReactComponent as CSS} from '../assets/Skills/css3.svg'
+import {ReactComponent as JavaScript} from '../assets/Skills/javascript.svg'
+import {ReactComponent as MongoDB} from '../assets/Skills/mongodb.svg'
+import {ReactComponent as Express} from '../assets/Skills/express.svg'
+import {ReactComponent as ReactLogo} from '../assets/Skills/react.svg'
+import {ReactComponent as NodeJS} from '../assets/Skills/nodedotjs.svg'
+import {ReactComponent as Sass} from '../assets/Skills/sass.svg'
+import {ReactComponent as Tailwind} from '../assets/Skills/tailwindcss.svg'
+import {ReactComponent as AdobeXD} from '../assets/Skills/adobexd.svg'
+import {ReactComponent as VSCode} from '../assets/Skills/visualstudiocode.svg'
+
+function Skills() {
+
+  const skills = [
+    <HTML className="fill-[#E34F26]"/>,
+    <CSS className="fill-[#1572B6]"/>,
+    <JavaScript className="fill-[#F7DF1E]"/>,
+    <MongoDB className="fill-[#47A248]"/>,
+    <Express className="fill-[#000000]"/>,
+    <ReactLogo className="fill-[#61DAFB]"/>,
+    <NodeJS className="fill-[#339933]"/>,
+    <Sass className="fill-[#CC6699]"/>,
+    <Tailwind className="fill-[#06B6D4]"/>,
+    <AdobeXD className="fill-[#FF61F6]"/>,
+    <VSCode className="fill-[#007ACC]"/>
+  ]
+  return (
+      <ul 
+      className="grid grid-cols-2 gap-[2rem] sm:flex sm:flex-wrap sm:max-w-[30rem] sm:justify-center lg:justify-start">
+          {
+              skills.map(skill => {
+                return (
+                    <li className="max-w-[2rem]">
+                        {skill}
+                    </li>
+                )
+            })
+          }
+          
+      </ul>
+  )
+}
+
+export default Skills;

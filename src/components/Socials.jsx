d55b401fed9b9cfca7a3ad5a31832cadab938e3a
Adrian Nads <marcelonads@gmail.com>
2022-02-26 13:46:46 +0800

    ADDED link for socials at Home page

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -6,12 +6,14 @@ import {ReactComponent as LinkedIn} from '../assets/socials/linkedin.svg'
 // Framer Motion
 import { motion } from 'framer-motion'
 
+/**
+ * Container of my social media accounts. */
 function Socials({ className }) {
 
     const socials = [
-        {logo: <Github title="github"/>, label: 'Github Account'},
-        {logo: <Twitter />, label: 'Twitter Account'},
-        {logo: <LinkedIn />, label: 'LinkedIn Account'}
+        {logo: <Github title="github"/>, label: 'Github Account', link: 'https://github.com/NADS-PROGRAMMER'},
+        {logo: <Twitter />, label: 'Twitter Account', link: 'https://twitter.com/NadsMarcelo'},
+        {logo: <LinkedIn />, label: 'LinkedIn Account', link: 'https://www.linkedin.com/in/adrian-marcelo-aaa19b228/'}
     ]
 
     return (
@@ -20,7 +22,7 @@ function Socials({ className }) {
                   socials.map((social, index) => {
                       return (
                           <motion.li key={index} className="social w-[1.5rem]">
-                              <a aria-label={social.label} href="">
+                              <a aria-label={social.label} href={social.link} target="_blank">
                                   {social.logo}
                               </a>
                           </motion.li>
